132e6437049906aeb7d04d4c41024e2aa5cccd9f
Maham Tanveer <maham.251989@gmail.com>
2017-01-19 14:54:47 +0100

    Multiple bottles.

--- src/pour_bottle_action_client.cpp
+++ src/pour_bottle_action_client.cpp
@@ -8,18 +8,38 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "pour_bottle_client");
 	Client client("pour_bottle", true); // true -> don't need ros::spin()
 	client.waitForServer();
-	project16_manipulation::PourBottleGoal goal;
 	
-	// Fill in goal here
+	project16_manipulation::PourBottleGoal goal;
+	project16_manipulation::PourBottleGoal goal2;
+		
+	// Fill in goal 1 here
 	goal.bottle_id = "bottle";
 	goal.portion_size = 10.0;
+		
+	// Fill in goal 1 here
+	goal2.bottle_id = "bottle_2";
+	goal2.portion_size = 10.0;
 	
+
+	// Send goal 1
 	client.sendGoal(goal);
 	client.waitForResult();
 	if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED && client.getResult()->success)
-		printf("Yay! The Bottle has been poured!");
+		printf("Yay! The Bottle 1 has been poured!");
     	else
-		printf("Noo! Something failed!");
+		printf("Noo! Something failed with pouring bottle 1!");
+	printf("Current State: %s\n", client.getState().toString().c_str());
+
+	
+	// Send goal 2
+	client.sendGoal(goal2);
+	client.waitForResult();
+	if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED && client.getResult()->success)
+		printf("Yay! The Bottle 2 has been poured!");
+    else
+		printf("Noo! Something failed with pouring bottle 2!");
 	printf("Current State: %s\n", client.getState().toString().c_str());
 	return 0;
+
+
 }
