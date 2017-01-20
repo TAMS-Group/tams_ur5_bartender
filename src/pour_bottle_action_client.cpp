535fd8f9549a3ce8ac11201ef9a0de7bcceb4daa
Henning Kayser <1kayser@informatik.uni-hamburg.de>
2017-01-20 16:30:33 +0100

    Using only bacardi bottle.

--- src/pour_bottle_action_client.cpp
+++ src/pour_bottle_action_client.cpp
@@ -10,16 +10,10 @@ int main(int argc, char** argv)
 	client.waitForServer();
 	
 	project16_manipulation::PourBottleGoal goal;
-	project16_manipulation::PourBottleGoal goal2;
 		
 	// Fill in goal 1 here
-	goal.bottle_id = "bottle";
+	goal.bottle_id = "bacardi";
 	goal.portion_size = 10.0;
-		
-	// Fill in goal 1 here
-	goal2.bottle_id = "bottle_2";
-	goal2.portion_size = 10.0;
-	
 
 	// Send goal 1
 	client.sendGoal(goal);
@@ -29,16 +23,7 @@ int main(int argc, char** argv)
     	else
 		printf("Noo! Something failed with pouring bottle 1!");
 	printf("Current State: %s\n", client.getState().toString().c_str());
-
 	
-	// Send goal 2
-	client.sendGoal(goal2);
-	client.waitForResult();
-	if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED && client.getResult()->success)
-		printf("Yay! The Bottle 2 has been poured!");
-    else
-		printf("Noo! Something failed with pouring bottle 2!");
-	printf("Current State: %s\n", client.getState().toString().c_str());
 	return 0;
 
 
