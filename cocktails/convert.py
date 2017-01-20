f01fcab3e8e2bfa11e7fa8ada2ba27012f4a2a29
Daniel Ahlers <12ahlers@informatik.uni-hamburg.de>
2017-01-20 15:43:16 +0100

    cocktail coordination implemented

---
+++ cocktails/convert.py
@@ -0,0 +1,22 @@
+f1 = open("cocktails.txt","r")
+f2 = open("../config/cocktails.yaml","w")
+
+f2.write("# Cocktails\n")
+f2.write("cocktails:\n")
+
+for line in f1:
+    if (line[1] == " "):
+	line = line.lstrip(" ")
+	line = line.rstrip("\n")
+	split = line.split(" ")
+        f2.write("        - incr:\n")
+        f2.write("            type: '" + " ".join(split[2:]) + "'\n")
+        f2.write("            amount: " + split[0] + "\n")
+    else:
+	f2.write("- cocktail:\n")
+	f2.write("    name: '" + line.rstrip("\n") + "'\n")
+	f2.write("    ingredients:\n")
+
+f1.close()
+f2.close()
+
--- src/tams_pr16_drink_chooser.cpp
+++ src/tams_pr16_drink_chooser.cpp
@@ -10,7 +10,7 @@
 #include <XmlRpcException.h>
 #include <actionlib/server/simple_action_server.h>
 #include <actionlib/client/simple_action_client.h>
-#include <project16_coordinator/cocktailAction.h>
+#include <project16_coordinator/CocktailAction.h>
 #include <project16_manipulation/PourBottleAction.h>
 
 
@@ -57,7 +57,10 @@ public:
 
         sub_ = nh.subscribe("recognizedObjects", 10, &DrinkChooser::objectsCallback, this);
 
-        pnh.getParam("cocktails", cocktails);
+        pnh.getParam("cocktails", cocktails_);
+
+        current_incr_ = 0;
+        max_incr_ = 0;
 
         //Register action service
         as_ = new actionlib::SimpleActionServer<project16_coordinator::CocktailAction>(nh, "cocktail_mixer", boost::bind(&DrinkChooser::mix, this, _1), false);
@@ -66,16 +69,16 @@ public:
 
 
         try {
-            for (int32_t i = 0; i < cocktails.size(); ++i) {
-                XmlRpc::XmlRpcValue& c = cocktails[i].begin()->second;
+            for (int32_t i = 0; i < cocktails_.size(); ++i) {
+                XmlRpc::XmlRpcValue& c = cocktails_[i].begin()->second;
                 std::map <std::string, double> incr;
 
                 for (int32_t j = 0; j < c["ingredients"].size(); ++j) {
-                    std::string name = c["ingredients"][i]["incr"]["type"];
-                    incr[name] = double(c["ingredients"][i]["incr"]["amount"]);
+                    std::string name = c["ingredients"][j]["incr"]["type"];
+                    incr[name] = double(c["ingredients"][j]["incr"]["amount"]);
                 }
                 Cocktail c1(i, c["name"], incr);
-                cocktails_db.push_back(c1);
+                cocktails_db_.push_back(c1);
             }
         } catch (XmlRpc::XmlRpcException & e) {
             ROS_WARN("%s", e.getMessage().c_str());
@@ -83,61 +86,109 @@ public:
     }
 
     void objectsCallback(const pr2016_msgs::BarCollisionObjectArrayConstPtr & msg) {
-        bottles.clear();
+        bottles_.clear();
         if (msg->objects.size()) {
             for (unsigned int i = 0; i < msg->objects.size(); i += 1) {
                 moveit_msgs::CollisionObject obj = msg->objects[i];
                 std::stringstream ss;
                 ss << obj.type.key;
                 std::string key = ss.str();
-                bottles.push_back(key);
+                bottles_.push_back(key);
             }
         }
     }
 
-    void execute(const project16_coordinator::CocktailConstPtr& goal) {
+    void mix(const project16_coordinator::CocktailGoalConstPtr& goal) {
         Cocktail ordered_cocktail;
         bool success = false;
 
-        for (int i = 0; i < cocktails_db.size(); i++) {
-            if (cocktails_db[i].getName() == goal->cocktail) {
-                ordered_cocktail = cocktails_db[i];
+        for (int i = 0; i < cocktails_db_.size(); i++) {
+            if (cocktails_db_[i].getName() == goal->cocktail) {
+                ordered_cocktail = cocktails_db_[i];
             }
         }
 
-        if (ordered_cocktail == NULL) {
+        if (ordered_cocktail.getName() == "") {
+            ROS_ERROR_STREAM("Cocktail '" << goal->cocktail << "' not found");
             as_->setAborted();
             return;
         }
-        feedback.task_state = "Start mixing " + ordered_cocktail.getName();
-        as_->publishFeedback(feedback);
+
+        actionlib::SimpleActionClient<project16_manipulation::PourBottleAction> ac("pour_bottle", true);
+        ac.waitForServer();
+        feedback_.task_state = "Start mixing " + ordered_cocktail.getName();
+        ROS_INFO_STREAM("Start mixing " << ordered_cocktail.getName());
+        as_->publishFeedback(feedback_);
 
         std::map<std::string, double> incr = ordered_cocktail.getIngredients();
-        for (int i = 0; i < incr.size(); i++) {
-            
+        std::map<std::string, double>::iterator it;
 
+
+        success_ = true;
+        max_incr_ = incr.size();
+        for (it = incr.begin(); (it != incr.end()) && success_; it++) {
+            project16_manipulation::PourBottleGoal goal;
+            current_incr_ = std::distance(incr.begin(), incr.find(it->first)) + 1;
+
+
+            std::stringstream ss;
+            ss << it->first;
+            std::string name = ss.str();
+            goal.bottle_id = name;
+            goal.portion_size = double(it->second);
+
+            ROS_INFO_STREAM("Mixing " << goal.portion_size << " cl " << goal.bottle_id);
+            ac.sendGoal(goal,
+                    boost::bind(&DrinkChooser::doneCB, this, _1, _2),
+                    boost::bind(&DrinkChooser::actCB, this),
+                    boost::bind(&DrinkChooser::feedCB, this, _1));
+            ac.waitForResult();
         }
 
-        if (success) {
-            feedback.task_state = "Finished mixing " + ordered_cocktail.getName();
-            as_->publishFeedback(feedback);
-            result_.success = true;
-            as_.setSucceeded(result_);
+        if (success_) {
+            feedback_.task_state = "Finished mixing " + ordered_cocktail.getName();
+            ROS_INFO_STREAM("Finished mixing " << ordered_cocktail.getName());
+        }else{
+            feedback_.task_state = "Failed mixing " + ordered_cocktail.getName();
+            ROS_INFO_STREAM("Failed mixing " << ordered_cocktail.getName());
         }
+        as_->publishFeedback(feedback_);
+        result_.success = success_;
+        as_->setSucceeded(result_);
     }
-}
 
+    void doneCB(const actionlib::SimpleClientGoalState& state, const project16_manipulation::PourBottleResultConstPtr& result) {
+        success_ = result->success;
+    }
+
+    void actCB() {
+
+    }
+
+    void feedCB(const project16_manipulation::PourBottleFeedbackConstPtr& feed) {
+        std::stringstream ss;
+        ss << current_incr_;
+        ss << "/";
+        ss << max_incr_;
+        std::string step = ss.str();
+        ROS_INFO_STREAM("" << step << " " << feed->task_state);
+        feedback_.task_state = "" + step + " " + feed->task_state;
+        as_->publishFeedback(feedback_);
+    }
 
 private:
-std::string mapFrameId_;
-std::string objFramePrefix_;
-ros::Subscriber sub_;
-XmlRpc::XmlRpcValue cocktails;
-std::vector <Cocktail> cocktails_db;
-std::vector <std::string> bottles;
-project16_coordinator::CocktailFeedback feedback_;
-project16_coordinator::CocktailResult result_;
-
+    std::string mapFrameId_;
+    std::string objFramePrefix_;
+    ros::Subscriber sub_;
+    XmlRpc::XmlRpcValue cocktails_;
+    std::vector <Cocktail> cocktails_db_;
+    std::vector <std::string> bottles_;
+    bool success_;
+    actionlib::SimpleActionServer<project16_coordinator::CocktailAction>* as_;
+    project16_coordinator::CocktailFeedback feedback_;
+    project16_coordinator::CocktailResult result_;
+    int current_incr_;
+    int max_incr_;
 };
 
 int main(int argc, char **argv) {
