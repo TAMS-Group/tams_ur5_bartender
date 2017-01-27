e1c6e7e66547d6510a4af263dfedf416d4deeb2e
Henning Kayser <1kayser@informatik.uni-hamburg.de>
2017-01-27 12:10:31 +0100

    Fixing build problem.

--- src/pour_bottle_action_client.cpp
+++ src/pour_bottle_action_client.cpp
@@ -10,6 +10,8 @@ int main(int argc, char** argv)
 	client.waitForServer();
 	
 	project16_manipulation::PourBottleGoal goal;
+	project16_manipulation::PourBottleGoal goal2;
+
 		
 	// Fill in goal 1 here
 	goal.bottle_id = "bacardi";
@@ -26,6 +28,15 @@ int main(int argc, char** argv)
 		printf("Yay! The Bottle 1 has been poured!");
     	else
 		printf("Noo! Something failed with pouring bottle 1!");
+	printf("Current State: %s\n", client.getState().toString().c_str());
+
+	// Send goal 2
+	client.sendGoal(goal2);
+	client.waitForResult();
+	if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED && client.getResult()->success)
+		printf("Yay! The Bottle 2 has been poured!");
+    	else
+		printf("Noo! Something failed with pouring bottle 2!");
 	printf("Current State: %s\n", client.getState().toString().c_str());
 	
 	return 0;
